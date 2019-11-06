0ea58cf02cf59230cdce5a3f9f689f66e062cf56
Rolando Antón <ranton@fortinet.com>
2019-11-06 10:54:53 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#update
+apt-get update -y &  apt-get upgrade -y & apt-get dist-upgrade -y
 
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
-#openbox-session&
 xfce4-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -forever &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
+cd /root/noVNC && ln -s vnc.html index.html && ./utils/launch.sh --vnc localhost:5900
